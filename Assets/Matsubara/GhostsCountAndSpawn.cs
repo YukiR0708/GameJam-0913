04fee7aeba5e1f699d88032d0670001958d8f7ea
MatubaraTooru <kurakura.ingen@gmail.com>
2022-09-13 15:30:15 +0900

    ゴーストの生成の修正

--- Assets/Matsubara/GhostsCountAndSpawn.cs
+++ Assets/Matsubara/GhostsCountAndSpawn.cs
@@ -10,23 +10,24 @@ public class GhostsCountAndSpawn : MonoBehaviour
     [Header("ç≈ëÂèoåªêî"), SerializeField] int _maxcount = 30;
     [Header("ç≈è¨èoåªêî"), SerializeField] int _mincount = 20;
     bool _timeLimit;
-    List<GameObject> _ghosts;
+    List<GhostsCountAndSpawn> _ghosts;
+    [SerializeField] TimeAndCount _count;
     private void Awake()
     {
         //_ghosts = new List<GameObject>();
     }
     void Start()
     {
-        _ghosts = GetComponent<TimeAndCount>()._ghostList;
-        _timeLimit = GetComponent<TimeAndCount>()._timeLimitToF;
+        //_ghosts = GetComponent<TimeAndCount>()._ghostList;
+        //_timeLimit = GetComponent<TimeAndCount>()._timeLimitToF;
         int spawnCount = Random.Range(_mincount, _maxcount);
         for (int i = 0; i < spawnCount; i++)
         {
             float x = Random.Range(_rangeB.position.x, _rangeA.position.x);
             float y = Random.Range(_rangeB.position.y, _rangeA.position.y);
 
-            Instantiate(_ghost, new Vector2(x, y), Quaternion.identity);
-            _ghosts.Add(GameObject.Find("Ghost"));
+            //Instantiate(_ghost, new Vector2(x, y), Quaternion.identity);
+            _count._ghostList.Add(Instantiate(_ghost, new Vector2(x, y), Quaternion.identity));
         }
     }
     private void Update()
