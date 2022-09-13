455402e698e568e33f6fb69c08d2323e88d11c6e
NakamuraTakuto <rulitaku@icloud.com>
2022-09-13 11:49:17 +0900

    管理用変数の宣言

---
+++ Assets/Nakamura/TimeAndCount.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeAndCount : MonoBehaviour
+{
+    /// <summary>制限時間</summary>
+    [Header("制限時間の設定"), SerializeField] public int _timeLimit = 30;
+    /// <summary>時間用変数</summary>
+    float _time = 0f;
+    /// <summary>Plyer1のCount変数</summary>
+    [Header("Player1のCount"), SerializeField] public int _p1Count = 0;
+    /// <summary>Player2のCount変数</summary>
+    [Header("Player2のCount"), SerializeField] public int _p2Count = 0;
+    /// <summary>お化けの管理用List</summary>
+    [Header("お化けのList"), SerializeField] public List<GameObject> _ghostList = new List<GameObject>();
+    /// <summary>お化けのカウント用変数</summary>
+    [Header("お化けのCount変数"), SerializeField] public int _ghostCount;
+    /// <summary>制限時間に達したらtrue </summary>
+    [Header("制限時間に達しているかどうか"), SerializeField] public bool _timeLimitToF = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _time += Time.deltaTime;
+    }
+}
